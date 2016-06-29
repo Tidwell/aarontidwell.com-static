81ee95bf7dc8236fed175dddcf90475394d366de
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2016-06-28 20:03:58 -0400

    add build

---
+++ build/assets/js/index.js
@@ -0,0 +1,39 @@
+(function() {
+	function toggle() {
+		$("#sidebar-wrapper").toggleClass("active");
+		return false;
+	}
+
+	function animate() {
+		if (location.pathname.replace(/^\//, '') !== this.pathname.replace(/^\//, '') ||
+			location.hostname !== this.hostname) {
+			return;
+		}
+		var target = $(this.hash);
+		var name = this.hash.slice(1);
+		if (!name) { return; }
+		target = target.length ? target : $('[name=' + name + ']');
+		if (target.length) {
+			$('html,body').stop().animate({
+				scrollTop: target.offset().top
+			}, 1000, function() {
+				window.location.hash = name;
+			});
+			return false;
+		}
+	}
+
+	function init() {
+		$("[menu-close]").click(toggle);
+		$("[menu-toggle]").click(toggle);
+		$('a[href^="#"]').click(animate);
+
+		$('.portfolio').on('click', '.card', function(e) {
+			if ($(e.target).is('a')) { return; } //do nothing if its a link
+			$(this).toggleClass('flipped');
+		});
+	}
+
+	// Scrolls to the selected menu item on the page
+	$(init);
+}());
